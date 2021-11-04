f1e3ce636f7d1187d0ab637be35411f2de45301e
Ali Amini <a.amini78@gmail.com>
2021-11-04 12:22:55 +0330

    clean the user controller code

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -15,15 +15,17 @@ use Helper\ValidateData;
  * controller for manage users
  */
 class UserController extends Controller
-{
+{   
+    protected UserRepository $user;
 
+        public function __construct() {
+            $this->user = new UserRepository();
+        }
     public function index()
     {
-        //instance user repository class
-        $Users = new UserRepository();
 
         // get all user data from db
-        $AllData =  $Users->GetAllItems();
+        $AllData =  $this->user->GetAllItems();
 
         //set all data for send to view
         $params = [
@@ -41,11 +43,9 @@ class UserController extends Controller
         $UserId = $_GET['id'];
 
 
-        //instance user class
-        $user = new UserRepository();
 
         //get user for edit 
-        $currentUser = $user->GetItem($UserId);
+        $currentUser = $this->user->GetItem($UserId);
 
 
         //$user->editItem($UserId,$currentUser);
@@ -62,8 +62,6 @@ class UserController extends Controller
     // POST -> EDIT USER
     public function editPost()
     {
-        //instance UserRepository
-        $user = new UserRepository();
 
         //get data from input
         $data = $_REQUEST;
@@ -79,7 +77,7 @@ class UserController extends Controller
         }
 
         //update user 
-        $result = $user->editItem($UserId, $data);
+        $result = $this->user->editItem($UserId, $data);
         
         if (!$result) {
             header("Location:/edit-user?id={$UserId}");
@@ -95,14 +93,12 @@ class UserController extends Controller
     public function delete()
     {
         //instance UserRepository
-        $user = new UserRepository();
-
         //get data from input
         $userId = $_GET['id'];
 
 
         //delete item
-        $result = $user->deleteItem($userId);
+        $result = $this->user->deleteItem($userId);
 
 
         if (!$result) {
